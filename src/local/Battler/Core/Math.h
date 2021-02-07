89f83c618e31e980f2a1e5447707619af91cf65e
Sasha7b9 <Sasha7b9@tut.by>
2021-02-07 16:26:02 +0300

    *

--- src/local/Battler/Core/Math.h
+++ src/local/Battler/Core/Math.h
@@ -8,6 +8,11 @@ public:
     /// Возвращает 1, если значение больше нуля, -1, если меньше, и 0, если val == 0
     template <class T> static int Sign(T val)
     {
-        if(val == T(0)) return 0; return val < 0 ? -1 : 1;
+        if(val == T(0))
+        {
+             return 0;
+        }
+
+        return val < 0 ? -1 : 1;
     }
 };
