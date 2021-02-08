c7c9c640c0e098cd039aeee75a20d395faeb1db7
Sasha7b9 <Sasha7b9@tut.by>
2021-02-08 17:32:44 +0300

    *

--- src/ThirdParty/opus/build_opus.bat
+++
@@ -1,3 +0,0 @@
-rmdir generated
-cmake . -Bgenerated -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR="Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release
-MSBuild.exe generated/Opus.sln /p:Configuration=release /t:build -clp:ErrorsOnly -nologo /m
