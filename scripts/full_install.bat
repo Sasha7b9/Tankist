ba163a0c2f9040100c072bf56c3370bfd6a5ef3b
Sasha7b9 <Sasha7b9@tut.by>
2021-02-09 14:10:45 +0300

    *

--- scripts/full_install.bat
+++ scripts/full_install.bat
@@ -1,15 +1,15 @@
 rem *** Create links ***
 
-rem set start_dir=%CD%
-rem cd install
-rem call create_links.bat
-rem cd %start_dir%
+set start_dir=%CD%
+cd install
+call create_links.bat
+cd %start_dir%
 
 rem *** Clone and build Urho3D ***
 
-rem cd install\Urho3D
-rem call assembly.bat full all
-rem cd %start_dir%
+cd install\Urho3D
+call assembly.bat build all
+cd %start_dir%
 
 rem *** Build third party ***
 
