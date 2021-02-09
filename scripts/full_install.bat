65c478e3bca10e10ca9f9f6805dfeabc36a974c9
Sasha7b9 <Sasha7b9@tut.by>
2021-02-09 15:09:54 +0300

    Начинаем избавляться от opus

--- scripts/full_install.bat
+++ scripts/full_install.bat
@@ -1,20 +1,25 @@
+rem Всё должно собираться static runtime
+
 rem *** Create links ***
 
-set start_dir=%CD%
-cd install
-call create_links.bat
-cd %start_dir%
+rem set start_dir=%CD%
+rem cd install
+rem call create_links.bat
+rem cd %start_dir%
 
 rem *** Clone and build Urho3D ***
 
-cd install\Urho3D
-call assembly.bat build all
-cd %start_dir%
+rem cd install\Urho3D
+rem call assembly.bat full all
+rem cd %start_dir%
 
 rem *** Build third party ***
 
-cd install\ThirdParty
-call make_build_all.bat
-cd %start_dir%
+rem cd install\ThirdParty
+rem call make_build_all.bat
+rem cd %start_dir%
 
 rem *** Build Tankist ***
+cd Windows
+call make.bat build all
+cd %start_dir%
