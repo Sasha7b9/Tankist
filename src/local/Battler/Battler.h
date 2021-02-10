3fb7c5e10495cc1c50ce66fd90fd506ee2c3e320
Sasha7b9 <Sasha7b9@tut.by>
2021-02-10 13:00:29 +0300

    refact

--- src/local/Battler/Battler.h
+++ src/local/Battler/Battler.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "Sample.h"
-
 namespace Urho3D
 {
     class Node;
@@ -17,9 +15,9 @@ class Vehicle;
 ///     - Defining attributes (including node and component references) of a custom component
 ///     (Saving and loading is broken now)
 
-class Battler : public Sample
+class Battler : public Application
 {
-    URHO3D_OBJECT(Battler, Sample);
+    URHO3D_OBJECT(Battler, Application);
 
 public:
     /// Construct.
@@ -28,7 +26,19 @@ public:
     /// Setup after engine initialization and before running the main loop.
     void Start() override;
 
+    void Setup() override;
+
 private:
+
+    /// Create logo.
+    void CreateLogo();
+
+    /// Set custom window Title & Icon
+    void SetWindowTitleAndIcon();
+
+    /// Create console and debug HUD.
+    void CreateConsoleAndDebugHud();
+
     /// Create static scene content.
     void CreateScene();
 
@@ -41,12 +51,42 @@ private:
     /// Subscribe to necessary events.
     void SubscribeToEvents();
 
+    /// Initialize mouse mode on non-web platform.
+    void InitMouseMode(MouseMode mode);
+
     /// Handle application update. Set controls to vehicle.
     void HandleUpdate(StringHash eventType, VariantMap& eventData);
 
     /// Handle application post-update. Update camera position after vehicle has moved.
     void HandlePostUpdate(StringHash eventType, VariantMap& eventData);
 
+    /// Handle key down event to process key controls common to all samples.
+    void HandleKeyDown(StringHash eventType, VariantMap &eventData);
+
+    /// Handle key up event to process key controls common to all samples.
+    void HandleKeyUp(StringHash eventType, VariantMap &eventData);
+
+    /// Handle request for mouse mode on web platform.
+    void HandleMouseModeRequest(StringHash eventType, VariantMap &eventData);
+
+    /// Handle request for mouse mode change on web platform.
+    void HandleMouseModeChange(StringHash eventType, VariantMap &eventData);
+
+    /// Handle scene update event to control camera's pitch and yaw for all samples.
+    void HandleSceneUpdate(StringHash eventType, VariantMap &eventData);
+
     /// The controllable vehicle component.
     WeakPtr<Vehicle> vehicle_;
+
+    /// Scene.
+    SharedPtr<Scene> scene_;
+
+    /// Camera scene node.
+    SharedPtr<Node> cameraNode_;
+
+    /// Logo sprite.
+    SharedPtr<Sprite> logoSprite_;
+
+    /// Mouse mode option to use in the sample.
+    MouseMode useMouseMode_;
 };
