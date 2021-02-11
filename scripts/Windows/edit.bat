f1e9ad1152e4198e7e97b3ce44986d7dc24cffa3
Sasha7b9 <Sasha7b9@tut.by>
2021-02-11 09:39:41 +0300

    refact

--- scripts/Windows/edit.bat
+++ scripts/Windows/edit.bat
@@ -9,11 +9,13 @@ goto SHOW_HINT
 :START_DEBUG
 @echo on
 start devenv.exe ../../generated/debug/TankistU3D/TankistU3D.sln /ProjectConfig Debug
+@echo off
 goto EXIT
 
 :START_RELEASE
 @echo on
 start devenv.exe ../../generated/release/TankistU3D/TankistU3D.sln /ProjectConfig Release
+@echo off
 goto EXIT
 
 :SHOW_HINT
