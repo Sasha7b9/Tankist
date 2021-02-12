a05e69295adef95692b6fb133a0082d76409a99c
Sasha7b9 <Sasha7b9@tut.by>
2021-02-12 09:57:06 +0300

    refact

--- src/local/Battler/stdafx.h
+++ src/local/Battler/stdafx.h
@@ -117,7 +117,7 @@ using namespace Urho3D;
 #include "GameInput.h"
 #include "GUI/Instructions.h"
 #include "GUI/Logo.h"
-#include "GUI/MouseT.h"
+#include "Input/MouseT.h"
 #include "Network/GameServer/_ClientServer.h"
 #include "Network/GameServer/_GameMessages.h"
 #include "Scene/_GameScene.h"
