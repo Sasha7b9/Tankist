4a178e1ec6f34853e53b0d8fe721808fe2321512
Sasha7b9 <Sasha7b9@tut.by>
2021-02-12 08:17:34 +0300

    *

--- src/local/Battler/Battler.h
+++ src/local/Battler/Battler.h
@@ -10,35 +10,35 @@ class Battler : public Application
     URHO3D_OBJECT(Battler, Application);
 
 public:
-    /// Construct.
+    // Construct.
     explicit Battler(Context* context);
 
-    /// Setup after engine initialization and before running the main loop.
+    // Setup after engine initialization and before running the main loop.
     virtual void Start() override;
 
     virtual void Setup() override;
 
-    /// Cleanup after the main loop. Called by Application.
+    // Cleanup after the main loop. Called by Application.
     virtual void Stop() override;
 
     void Exit();
 
 private:
 
-    /// Set custom window Title & Icon
+    // Set custom window Title & Icon
     void SetWindowTitleAndIcon();
 
-    /// Subscribe to necessary events.
+    // Subscribe to necessary events.
     void SubscribeToEvents();
 
     WeakPtr<Logo> logo;
 
-    /// Handle application update. Set controls to vehicle.
+    // Handle application update. Set controls to vehicle.
     void HandleUpdate(StringHash eventType, VariantMap &eventData);
 
-    /// Handle application post-update. Update camera position after vehicle has moved.
+    // Handle application post-update. Update camera position after vehicle has moved.
     void HandlePostUpdate(StringHash eventType, VariantMap &eventData);
 
-    /// Handle scene update event to control camera's pitch and yaw for all samples.
+    // Handle scene update event to control camera's pitch and yaw for all samples.
     void HandleSceneUpdate(StringHash eventType, VariantMap &eventData);
 };
