d46329b88a53c12f60cf2690921a9e4774d7d0d6
Sasha7b9 <Sasha7b9@tut.by>
2021-02-14 18:29:23 +0300

    *

--- src/Install.cmake
+++ src/Install.cmake
@@ -1,15 +1,15 @@
 if(${CMAKE_BUILD_TYPE} STREQUAL Debug)
     if(WIN32)
-        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../out/debug"
+        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../out/debug")
     else()
-        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../../../out/debug"
+        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../../../out/debug")
     endif()
 )
 else()
     if(WIN32)
-        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../out/release"
+        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../out/release")
     else()
-        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../../../out/release"
+        install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "${CMAKE_BINARY_DIR}/../../../out/release")
     endif()
 )
 endif()
